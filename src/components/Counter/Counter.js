3f3fede5e0a6c6c8b8cc2a57b914872c6ca93afe
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-18 12:02:15 +0200

    added components Counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="Counter">
+        <span className="Counter__value">0</span>
+
+        <div className="Counter__controls">
+          <button type="button">Збільшити на 1</button>
+          <button type="button">Зменшити на 1</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
