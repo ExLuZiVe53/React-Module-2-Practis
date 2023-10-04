b8eb6d1a875ce193d4dfacbbe2aef6e450101c62
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-04 13:21:27 +0200

    Create DeviceCard.jsx

---
+++ src/components/DeviceCard/DeviceCard.jsx
@@ -0,0 +1,13 @@
+const DeviceCard = () => {
+  return (
+    <div>
+      <img src="#" alt="" />
+      <h3></h3>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  );
+};
+
+export default DeviceCard;
